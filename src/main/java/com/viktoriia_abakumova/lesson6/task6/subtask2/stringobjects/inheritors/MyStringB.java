65a583b4181d59db6eb24d882308e9018abd5497
Victoria Abakumova <abakumovaviktory@gmail.com>
2019-12-31 11:22:43 +0200

    Task_6 (files were added)

---
+++ src/main/java/com/viktoriia_abakumova/lesson6/task6/subtask2/stringobjects/inheritors/MyStringB.java
@@ -0,0 +1,25 @@
+package com.viktoriia_abakumova.lesson6.task6.subtask2.stringobjects.inheritors;
+
+import com.viktoriia_abakumova.lesson6.task6.subtask2.stringobjects.MyString;
+
+public class MyStringB extends MyString {
+
+    public MyStringB(String string) {
+        super(string);
+    }
+
+    @Override
+    public int hashCode() {
+        int sum = 0;
+        char[] chars = string.toCharArray();
+        for (int i = 0; i < 4; i++) {
+            sum += chars[i];
+        }
+        return sum;
+    }
+
+    @Override
+    public String toString() {
+        return this.string;
+    }
+}
